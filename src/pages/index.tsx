97a787213e7b7a51a41429b564eefc5a5f1f4cf4
Yuukin256 <Yuukin256@gmail.com>
2022-04-24 13:04:57 +0900

    react, next 導入

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import type { FC } from 'react';
+
+const Component: FC = () => <div>Hello, World!</div>;
+
+const Page: NextPage = () => <Component />;
+
+export default Page;
